2798e849ef414f37e2550a03a27d23701769913d
JustinChing30 <justin_ethan_l_ching@dlsu.edu.ph>
2025-07-22 21:03:30 +0800

    changed likes -> likers. this will store user_ids that liked

--- peer2.py
+++ peer2.py
@@ -436,21 +436,22 @@ class LSNPPeer:
         msg = self.message_builder.build_post(content, ttl_seconds)
         current_time = time.time()
 
-        # TO UPDATE for MS2 :: must be in followers
+        # TODO: check if correct
         peers_to_send_to = [uid for uid in self.followers if uid != self.user_id]
         if peers_to_send_to:
             for uid in peers_to_send_to:
                 self.send_message_to_peer(uid, msg)
-            self.posts[current_time] = {
-                "sender": self.user_id,
-                "content": content,
-                "likes": 0
-            }
         else:  # If no other peers, broadcast
             self.sock.sendto(msg.encode(), (self.BROADCAST_IP, self.PORT))
             self.stats['messages_sent'] += 1
             if self.verbose:
                 display_manager.log_debug(f"Broadcasted POST: {content}")
+
+        self.posts[current_time] = {
+            "sender": self.user_id,
+            "content": content,
+            "likers": set() # set of user_ids that liked this post
+        }
 
         print(f"Post sent: {content}")
 
