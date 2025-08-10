db41bc6a1f94f0e9516b94aba05276eeb4e9efac
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-08-10 22:13:04 +0800

    added back discovery loop on start

--- peer2.py
+++ peer2.py
@@ -106,6 +106,7 @@ class LSNPPeer:
         """Start the peer"""
         self.running = True
         threading.Thread(target=self._listen_loop, daemon=True).start()
+        threading.Thread(target=self._discovery_loop, daemon=True).start()
         self.broadcast_profile()
 
     def stop(self):
