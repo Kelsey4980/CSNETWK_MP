0065e4a6ccd6bc0c2103502ed1edc68a801948da
haniellejermayn <chua.haniellejermayn@gmail.com>
2025-08-11 19:41:40 +0800

    removed duplicate post message

--- peer2.py
+++ peer2.py
@@ -1308,8 +1308,6 @@ class LSNPPeer:
             "likers": set() # set of user_ids that liked this post
         }
 
-        print(f"Post sent: {content}")
-
     def send_dm(self, target_user_id, content):
         """Send a DM to a specific user"""
         if not content.strip():
